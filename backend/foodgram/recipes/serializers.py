43461ee4c0b5d9d8b027ff9b6e9cbc55f03972eb
vshkinder <shkinder123@gmail.com>
2022-08-13 20:06:58 +0300

    fix recipe create bug

--- backend/foodgram/recipes/serializers.py
+++ backend/foodgram/recipes/serializers.py
@@ -246,7 +246,7 @@ class RecipeSerializer(serializers.ModelSerializer):
 
     def get_ingredients(self, obj):
         ingredients = obj.ingredients.values(
-            'id', 'name', 'measurement_unit', amount=F('recipe__amount')
+            'id', 'name', 'measurement_unit', amount=F('recipes__amount')
         )
         return ingredients
 
