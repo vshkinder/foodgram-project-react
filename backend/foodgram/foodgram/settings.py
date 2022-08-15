715e3bdca5d5c029b8ff080ebd16055e59c4b691
vshkinder <shkinder123@gmail.com>
2022-08-15 09:19:53 +0300

    fix recipe create bug

--- backend/foodgram/foodgram/settings.py
+++ backend/foodgram/foodgram/settings.py
@@ -17,7 +17,6 @@ load_dotenv()
 
 AUTH_USER_MODEL = 'users.CustomUser'
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
