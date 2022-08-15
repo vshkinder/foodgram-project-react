4706ec49df98f63af8d3fce788bd1a6de724f6be
vshkinder <shkinder123@gmail.com>
2022-08-15 12:59:18 +0300

    fix recipe create bug

--- backend/foodgram/recipes/filters.py
+++ backend/foodgram/recipes/filters.py
@@ -1,53 +1,53 @@
-from django.core.exceptions import ValidationError
-import django_filters as filters
-
-from .models import Recipe, Tag, Ingredient
-from users.models import CustomUser
-
-
-class TagsMultipleChoiceField(
-        filters.fields.MultipleChoiceField):
-    def validate(self, value):
-        if self.required and not value:
-            raise ValidationError(
-                self.error_messages['required'],
-                code='required')
-        for val in value:
-            if val in self.choices and not self.valid_value(val):
-                raise ValidationError(
-                    self.error_messages['invalid_choice'],
-                    code='invalid_choice',
-                    params={'value': val},)
-
-
-class TagsFilter(filters.AllValuesMultipleFilter):
-    field_class = TagsMultipleChoiceField
-
-
-class IngredientFilter(filters.FilterSet):
-    name = filters.CharFilter(lookup_expr='istartswith')
-
-    class Meta:
-        model = Ingredient
-        fields = ('name',)
-
-
-class RecipeFilter(filters.FilterSet):
-    author = filters.ModelChoiceFilter(
-        queryset=CustomUser.objects.all())
-    is_in_shopping_cart = filters.BooleanFilter(
-        widget=filters.widgets.BooleanWidget(),
-        label='В корзине.')
-    is_favorited = filters.BooleanFilter(
-        widget=filters.widgets.BooleanWidget(),
-        label='В избранных.')
-    tags = filters.AllValuesMultipleFilter(
-        field_name='tags__slug',
-        label='Ссылка')
-
-    class Meta:
-        model = Recipe
-        fields = ['is_favorited', 'is_in_shopping_cart', 'author', 'tags']
+#from django.core.exceptions import ValidationError
+#import django_filters as filters
+#
+#from .models import Recipe, Tag, Ingredient
+#from users.models import CustomUser
+#
+#
+#class TagsMultipleChoiceField(
+#        filters.fields.MultipleChoiceField):
+#    def validate(self, value):
+#        if self.required and not value:
+#            raise ValidationError(
+#                self.error_messages['required'],
+#                code='required')
+#        for val in value:
+#            if val in self.choices and not self.valid_value(val):
+#                raise ValidationError(
+#                    self.error_messages['invalid_choice'],
+#                    code='invalid_choice',
+#                    params={'value': val},)
+#
+#
+#class TagsFilter(filters.AllValuesMultipleFilter):
+#    field_class = TagsMultipleChoiceField
+#
+#
+#class IngredientFilter(filters.FilterSet):
+#    name = filters.CharFilter(lookup_expr='istartswith')
+#
+#    class Meta:
+#        model = Ingredient
+#        fields = ('name',)
+#
+#
+#class RecipeFilter(filters.FilterSet):
+#    author = filters.ModelChoiceFilter(
+#        queryset=CustomUser.objects.all())
+#    is_in_shopping_cart = filters.BooleanFilter(
+#        widget=filters.widgets.BooleanWidget(),
+#        label='В корзине.')
+#    is_favorited = filters.BooleanFilter(
+#        widget=filters.widgets.BooleanWidget(),
+#        label='В избранных.')
+#    tags = filters.AllValuesMultipleFilter(
+#        field_name='tags__slug',
+#        label='Ссылка')
+#
+#    class Meta:
+#        model = Recipe
+#        fields = ['is_favorited', 'is_in_shopping_cart', 'author', 'tags']
 
 
 #class RecipeFilter(filters.FilterSet):
