f29b10629b4bfa19832fd19ad37bca912198c013
vshkinder <shkinder123@gmail.com>
2022-08-16 09:00:45 +0300

    fix recipe create bug

--- backend/foodgram/recipes/serializers.py
+++ backend/foodgram/recipes/serializers.py
@@ -172,34 +172,19 @@ class RecipeSerializer(serializers.ModelSerializer):
         return recipe
 
     def update(self, instance, validated_data):
-        context = self.context['request']
-        ingredients = validated_data.pop('recipe_ingredients')
-        tags_set = context.data['tags']
-        recipe = instance
+        instance.image = validated_data.get('image', instance.image)
         instance.name = validated_data.get('name', instance.name)
         instance.text = validated_data.get('text', instance.text)
         instance.cooking_time = validated_data.get(
             'cooking_time', instance.cooking_time
         )
-        instance.image = validated_data.get('image', instance.image)
+        instance.tags.clear()
+        tags_data = self.initial_data.get('tags')
+        instance.tags.set(tags_data)
+        CountOfIngredient.objects.filter(recipe=instance).all().delete()
+        self.create_ingredients(validated_data.get('ingredients'), instance)
         instance.save()
-        instance.tags.set(tags_set)
-        CountOfIngredient.objects.filter(recipe=instance).delete()
-        ingredients_req = context.data['ingredients']
-        for ingredient in ingredients_req:
-            ingredient_model = Ingredient.objects.get(id=ingredient['id'])
-            CountOfIngredient.objects.create(
-                recipe=recipe,
-                ingredient=ingredient_model,
-                amount=ingredient['amount'],
-            )
         return instance
-
-    def to_representation(self, instance):
-        response = super(RecipeSerializer, self).to_representation(instance)
-        if instance.image:
-            response['image'] = instance.image.url
-        return response
 
 
 class SimpleRecipeSerializer(serializers.ModelSerializer):
