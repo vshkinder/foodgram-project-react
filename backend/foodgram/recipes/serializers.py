12bac440e95d39ecd558e0a26a28e56191c77078
vshkinder <shkinder123@gmail.com>
2022-08-16 22:21:08 +0300

    fix recipe create bug

--- backend/foodgram/recipes/serializers.py
+++ backend/foodgram/recipes/serializers.py
@@ -100,6 +100,7 @@ class RecipeSerializer(serializers.ModelSerializer):
     author = AuthorSerializer(read_only=True)
     ingredients = CountOfIngredientSerializer(
         many=True,
+        source='CountOfIngredient_set',
         read_only=True,
     )
     is_favorited = SerializerMethodField()
