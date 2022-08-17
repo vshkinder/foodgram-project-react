bf2d0cec2975ef740462a7c4ba7f7ac9c286af94
vshkinder <shkinder123@gmail.com>
2022-08-17 11:04:26 +0300

    fix recipe create bug

--- backend/foodgram/recipes/filters.py
+++ backend/foodgram/recipes/filters.py
@@ -6,37 +6,37 @@ from users.models import CustomUser
 
 
 class RecipeFilter(filters.FilterSet):
-    author = filters.ModelChoiceFilter(
-        queryset=CustomUser.objects.all())
+    #    author = filters.ModelChoiceFilter(
+    #        queryset=CustomUser.objects.all())
+    #    is_in_shopping_cart = filters.BooleanFilter(
+    #        widget=BooleanWidget(),
+    #        label='В корзине.')
+    #    is_favorited = filters.BooleanFilter(
+    #        widget=BooleanWidget(),
+    #        label='В избранных.')
+    #    tags = filters.AllValuesMultipleFilter(
+    #        field_name='tags__slug',
+    #        label='Ссылка')
+    #
+    #    class Meta:
+    #        model = Recipe
+    #        fields = ['author', 'tags']
+    tags = filters.AllValuesMultipleFilter(field_name='tags__slug')
+    author = filters.ModelChoiceFilter(queryset=CustomUser.objects.all())
+    is_favorited = filters.BooleanFilter(method='filter_is_favorited')
     is_in_shopping_cart = filters.BooleanFilter(
-        widget=BooleanWidget(),
-        label='В корзине.')
-    is_favorited = filters.BooleanFilter(
-        widget=BooleanWidget(),
-        label='В избранных.')
-    tags = filters.AllValuesMultipleFilter(
-        field_name='tags__slug',
-        label='Ссылка')
+        method='filter_is_in_shopping_cart')
+
+    def filter_is_favorited(self, queryset, name, value):
+        if value and not self.request.user.is_anonymous:
+            return queryset.filter(recipes_favorite__user=self.request.user)
+        return queryset
+
+    def filter_is_in_shopping_cart(self, queryset, name, value):
+        if value and not self.request.user.is_anonymous:
+            return queryset.filter(shopping_cart__user=self.request.user)
+        return queryset
 
     class Meta:
         model = Recipe
-        fields = ['author', 'tags']
-#    tags = filters.AllValuesMultipleFilter(field_name='tags__slug')
-#    author = filters.ModelChoiceFilter(queryset=CustomUser.objects.all())
-#    is_favorited = filters.BooleanFilter(method='filter_is_favorited')
-#    is_in_shopping_cart = filters.BooleanFilter(
-#        method='filter_is_in_shopping_cart')
-#
-#    def filter_is_favorited(self, queryset, name, value):
-#        if value and not self.request.user.is_anonymous:
-#            return queryset.filter(recipes_favorite__user=self.request.user)
-#        return queryset
-#
-#    def filter_is_in_shopping_cart(self, queryset, name, value):
-#        if value and not self.request.user.is_anonymous:
-#            return queryset.filter(shopping_cart__user=self.request.user)
-#        return queryset
-#
-#    class Meta:
-#        model = Recipe
-#        fields = ('tags', 'author')
+        fields = ('tags', 'author')
